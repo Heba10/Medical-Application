1f639cf8868421b55235469db1ff4b892563efbc
Heba10 <hebe10@yahoo.com>
2021-01-24 18:06:58 +0200

    ScreeningForm js

---
+++ js/ScreeningForm.js
@@ -0,0 +1,11 @@
+function Head() {
+    document.querySelector('#shoulder').style.display = 'none';
+    document.querySelector('#head').style.display = 'block';
+    document.querySelector('#confirm').style.display = 'block';
+}
+
+function shoulder() {
+    document.querySelector('#head').style.display = 'none';
+    document.querySelector('#shoulder').style.display = 'block';
+    document.querySelector('#confirm').style.display = 'block';
+}
